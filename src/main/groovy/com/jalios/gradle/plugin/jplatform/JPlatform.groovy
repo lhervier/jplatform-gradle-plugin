648cdae8e936f149ef11457cafecc0767d7cd26a
Lionel HERVIER <lionel.hervier@laposte.net>
2018-06-18 17:22:39 +0200

    Nettoyage d'une classe

--- src/main/groovy/com/jalios/gradle/plugin/jplatform/JPlatform.groovy
+++ src/main/groovy/com/jalios/gradle/plugin/jplatform/JPlatform.groovy
@@ -4,14 +4,19 @@ import org.gradle.api.Project
 
 class JPlatform {
 
+	/**
+	 * List of all loaded modules
+	 */
 	private def jModules = [:]
 	
+	/**
+	 * Root folder of the JPlatform
+	 */
 	final File rootFolder
 	
 	/**
 	 * Constructor
-	 * @param project current project
-	 * @param rootPath the root path
+	 * @param rootFolder path to the root folder of JPlatform
 	 */
 	JPlatform(File rootFolder) {
 		this.rootFolder = rootFolder
@@ -20,19 +25,12 @@ class JPlatform {
 	/**
 	 * Return the given module
 	 * @param name the name of the module
+	 * @return the module
 	 */
 	JModule module(String name) {
 		if( this.jModules[name] != null )
 			return this.jModules[name]
 		this.jModules[name] = new JModule(this.rootFolder, name)
 		return this.jModules[name]
-	}
-	
-	String getTypePath(String name) {
-		String path = "WEB-INF/data/types/${name}.xml"
-		File typeFile = new File(this.rootFolder, path)
-		if( !typeFile.exists() )
-			return null
-		return path
 	}
 }
