2ec61c7958826393151ace9f1c802a9500db6c22
Lionel HERVIER <lionel.hervier@laposte.net>
2018-07-05 12:20:04 +0200

    Added unit tests for trivial classes

---
+++ src/test/groovy/com/jalios/gradle/plugin/jplatform/source/impl/TestPluginXmlExtractor.groovy
@@ -0,0 +1,44 @@
+package com.jalios.gradle.plugin.jplatform.source.impl
+
+import static org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+import com.jalios.gradle.plugin.jplatform.JModule
+import com.jalios.gradle.plugin.test.InMemoryJFileSystem
+import com.jalios.gradle.plugin.test.util.ByteUtils
+
+class TestPluginXmlExtractor {
+
+	InMemoryJFileSystem fs
+	JModule module
+	PluginXmlExtractor extractor
+	
+	@Before
+	void before() {
+		this.fs = new InMemoryJFileSystem()
+		this.fs.addFile(
+				"WEB-INF/plugins/TestPlugin/plugin.xml",
+				ByteUtils.extractBytes("""
+					<plugin name="TestASIPlugin">
+					</plugin>
+				""")
+		)
+		this.module = new JModule(
+			"TestPlugin",
+			this.fs
+		)
+		this.extractor = new PluginXmlExtractor()
+	}
+	
+	@Test
+	void whenExtracting_thenFileExtracted() {
+		this.module.init(null, [this.extractor])
+		
+		assert module.paths.size() == 1
+		assert module.paths[0] == "WEB-INF/plugins/TestPlugin/plugin.xml"
+	}
+	
+	
+}
