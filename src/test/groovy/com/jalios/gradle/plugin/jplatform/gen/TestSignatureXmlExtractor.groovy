f0901927a11c541006ad8c94f6ce2b73237d877d
Lionel HERVIER <lionel.hervier@laposte.net>
2018-07-05 11:28:19 +0200

    Unit test for SignatureXmlExtractor

---
+++ src/test/groovy/com/jalios/gradle/plugin/jplatform/gen/TestSignatureXmlExtractor.groovy
@@ -0,0 +1,44 @@
+package com.jalios.gradle.plugin.jplatform.gen
+
+import static org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+import com.jalios.gradle.plugin.jplatform.JModule
+import com.jalios.gradle.plugin.jplatform.gen.impl.SignatureXmlExtractor
+import com.jalios.gradle.plugin.test.InMemoryJFileSystem
+import com.jalios.gradle.plugin.test.util.ByteUtils
+
+class TestSignatureXmlExtractor {
+
+	InMemoryJFileSystem fs
+	JModule module
+	SignatureXmlExtractor extractor
+	
+	@Before
+	void before() {
+		this.fs = new InMemoryJFileSystem()
+		this.fs.addFile(
+				"WEB-INF/plugins/TestPlugin/plugin.xml",
+				ByteUtils.extractBytes("""
+					<plugin name="TestASIPlugin">
+					</plugin>
+				""")
+		)
+		this.module = new JModule(
+			"TestPlugin",
+			this.fs
+		)
+		this.extractor = new SignatureXmlExtractor()
+	}
+	
+	@Test
+	void whenExtracting_thenExtracted() {
+		this.module.init([this.extractor], null)
+		
+		assert module.generatedPaths.size() == 1
+		assert module.generatedPaths[0].path == "WEB-INF/plugins/TestPlugin/signature.xml"
+		assert module.generatedPaths[0].source == "WEB-INF/plugins/TestPlugin/plugin.xml"
+	}
+}
